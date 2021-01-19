0be2a98ffd5d68e83853c690753dbda3d2e6aa4e
kasatkin <devkasatkin87@gmail.com>
2021-01-19 20:23:43 +0200

    Added diferent type of walls

--- core/src/com/devkasatkin87/game/tanks/Map.java
+++ core/src/com/devkasatkin87/game/tanks/Map.java
@@ -4,32 +4,86 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 
 public class Map {
+    private enum WallType {
+        HARD(0, 5, true), SOFT(1, 3, true), INDESTRUCTABLE(2,1,false), NONE(0,0, false);
+
+        int index;
+        int maxHp;
+        boolean desctructable;
+
+        WallType(int index, int maxHp, boolean desctructable) {
+            this.index = index;
+            this.maxHp = maxHp;
+            this.desctructable = desctructable;
+        }
+    }
+
+    private class Cell {
+        WallType type;
+        int hp;
+
+        public Cell(WallType type) {
+            this.type = type;
+            this.hp = type.maxHp;
+        }
+
+        public void damage() {
+            if (type.desctructable) {
+                hp--;
+                if (hp <= 0) {
+                    type = WallType.NONE;
+                }
+            }
+        }
+
+        public void changeType(WallType type) {
+            this.type = type;
+            hp = type.maxHp;
+        }
+    }
+
     public static final int SIZE_X = 64;
     public static final int SIZE_Y = 36;
     public static final int CELL_SIZE = 20;
 
     private TextureRegion textureGrass;
     private TextureRegion textureBlock;
-    private int[][] obstacles;
+    private TextureRegion[][] textureWalls;
+    private Cell[][] cells;
 
     public Map(TextureAtlas atlas) {
+        this.textureWalls = new TextureRegion(atlas.findRegion("Walls")).split(CELL_SIZE, CELL_SIZE);
         this.textureGrass = atlas.findRegion("Grass");
-        this.textureBlock = atlas.findRegion("Block");
-        this.obstacles = new int[SIZE_X][SIZE_Y];
+        this.cells = new Cell[SIZE_X][SIZE_Y];
         createMap();
     }
 
     private void createMap() {
         for (int i = 0; i < SIZE_X; i++) {
             for (int j = 0; j < SIZE_Y; j++) {
+                cells[i][j] = new Cell(WallType.NONE);
                 int cx = (int)(i / 4);
                 int cy = (int)(j / 4);
                 if (cx % 2 == 0 && cy % 2 == 0) {
-                    obstacles[i][j] = 5;
+                    if (MathUtils.random() < 0.8f) {
+                        cells[i][j].changeType(WallType.HARD);
+                    } else {
+                        cells[i][j].changeType(WallType.SOFT);
+                    }
                 }
             }
+        }
+        for (int i = 0; i < SIZE_X; i++) {
+            cells[i][0].changeType(WallType.INDESTRUCTABLE);
+            cells[i][SIZE_Y - 1].changeType(WallType.INDESTRUCTABLE);
+
+        }
+        for (int i = 0; i < SIZE_Y; i++) {
+            cells[0][i].changeType(WallType.INDESTRUCTABLE);
+            cells[SIZE_X - 1][i].changeType(WallType.INDESTRUCTABLE);
         }
     }
 
@@ -38,8 +92,8 @@ public class Map {
         int cy = (int) (bullet.getPosition().y / CELL_SIZE);
 
         if (cx >= 0 && cy >= 0 && cx < SIZE_X && cy < SIZE_Y ) {
-            if (obstacles[cx][cy] > 0) {
-                obstacles[cx][cy] -= bullet.getDamage();
+            if (cells[cx][cy].type != WallType.NONE) {
+                cells[cx][cy].damage();
                 bullet.deactivate();
             }
         }
@@ -70,7 +124,7 @@ public class Map {
 
         for (int i = leftX; i <= rightX; i++) {
             for (int j = bottomY; j <= topY ; j++) {
-                if (obstacles[i][j] > 0) {
+                if (cells[i][j].type != WallType.NONE) {
                     return false;
                 }
             }
@@ -88,8 +142,8 @@ public class Map {
 
         for (int i = 0; i < SIZE_X; i++) {
             for (int j = 0; j < SIZE_Y; j++) {
-                if (obstacles[i][j] > 0) {
-                    batch.draw(textureBlock, i * CELL_SIZE, j * CELL_SIZE);
+                if (cells[i][j].type != WallType.NONE) {
+                    batch.draw(textureWalls[cells[i][j].type.index][cells[i][j].hp - 1], i * CELL_SIZE, j * CELL_SIZE);
                 }
             }
         }
